e69c62c13b12e7dde6fe59f27c764157281b6f0f
Leekunhee830 <gorrka159@naver.com>
2020-10-31 17:44:02 +0900

    게시물 추가,게시물 목록구현

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,38 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Test {
     public static void main(String[] args) {
-		System.out.println("안녕하세요");
-	}
+    	Scanner sc=new Scanner(System.in);
+    	HashMap<String,String> amp=new HashMap<String,String>();
+    	String title="";
+    	String content="";
+    	
+ 
+    	while(true) {
+    		System.out.print("명령어를 입력해주세요:");
+            String str1=sc.nextLine();
+            if(str1.equals("exit")) {
+            	System.out.println("프로그램을 종료 합니다.");
+            	break;
+            }
+            if(str1.equals("add")) {
+            	System.out.println("게시물 제목을 입력해주세요:");
+            	title=sc.nextLine();
+            	System.out.println("게시물 내용을 입력해주세요:");
+            	content=sc.nextLine();
+            	amp.put(title,content);
+            	System.out.println("게시물이 등록되었습니다.");
+            }
+            if(str1.equals("list")) {
+            	Set set=amp.keySet();
+            	Iterator it=set.iterator();
+            	while(it.hasNext()) {
+            		System.out.println(it.next());
+            	}
+            }
+    	}	
+    }
 }
